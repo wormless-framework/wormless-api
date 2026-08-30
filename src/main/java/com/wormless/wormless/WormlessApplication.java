6c9d3233c2f46a1a9c901f7eb764ecc334bd2a19
thigoai <tfreire352@gmail.com>
2026-08-29 22:34:07 -0300

    ativando async e feign

--- src/main/java/com/wormless/wormless/WormlessApplication.java
+++ src/main/java/com/wormless/wormless/WormlessApplication.java
@@ -1,13 +1,13 @@
-package com.wormless.wormless;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
+@EnableFeignClients
 @SpringBootApplication
 public class WormlessApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(WormlessApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(WormlessApplication.class, args);
+    }
 }
