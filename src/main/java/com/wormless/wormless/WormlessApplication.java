340d9cb9b9a496edec9ebbc65a3d95bebd1c9514
thigoai <tfreire352@gmail.com>
2026-08-29 22:28:41 -0300

    initial spring boot setup

---
+++ src/main/java/com/wormless/wormless/WormlessApplication.java
@@ -0,0 +1,13 @@
+package com.wormless.wormless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WormlessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WormlessApplication.class, args);
+	}
+
+}
